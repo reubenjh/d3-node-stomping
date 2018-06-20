46a5b1f2a04ecb3c709c113d4a9dd4c7e22cbbe6
anton <anton.arus@gmail.com>
2018-06-20 18:45:59 +1200

    mucked around a bit more, bound two sets of svgs to the same data, turned data into array of objects

--- routes.js
+++ routes.js
@@ -5,10 +5,11 @@ const router = express.Router()
 
 router.get('/', (req, res) => {
   const d3n = d3NMaker.newD3N()
+  // console.log(d3n.document.body);
   const d3 = d3n.d3
         // doc = d3.document
 
-  let data = [10, 20, 3, 5, 7]
+  let data = [{value: 10}, {value: 20}, {value: 3}, {value: 5}, {value: 7}]
 
   let svg = d3.select(d3n.document.body)
     .select('#bones')
@@ -16,15 +17,32 @@ router.get('/', (req, res) => {
     .attr("height", 1000)
     .attr("width", 1000)
 
-  svg.selectAll('circle')
-    .data(data)
-    .enter().append('circle')
-      .attr("cx", () => 200 + Math.random()*100)
-      .attr("cy", (d,i) => 100*(i+1))
-      .attr("r", d => 2*d)
-      .attr("fill", () => "red")
+  let rest = svg.selectAll('circle')
+              .data(data)
+              .attr('cx', () => {
+                console.log('already a circle here');
+              })
+              .enter().append('circle')
+              .attr("cx", () => 200 + Math.random()*100)
+              .attr("cy", (d,i) => 100*(i+1))
+              .attr("r", d => 2*d.value*(1+Math.random()*0.3))
+              .attr("fill", () => "red")
+
+              svg.selectAll('rect')
+                          .data(data)
+                          .attr('cx', () => {
+                            console.log('already a circle here');
+                          })
+                          .enter().append('rect')
+                          .attr("x", () => 400 + Math.random()*100)
+                          .attr("y", (d,i) => 100*(i+1))
+                          .attr("width", d => 2*d.value*(1+Math.random()*0.3))
+                          .attr("height", d => 2*d.value*(1+Math.random()*0.3))
+                          .attr("fill", () => "red")
 
 
+
+  console.log(d3n.html());
   res.send(d3n.html())
 })
 
